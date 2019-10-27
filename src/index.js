223101bc923c9aad716742049c1c343d7e2150ff
jorge1g <jsgonzaga@uol.com.br>
2019-10-27 10:03:02 -0300

    Reinicio

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// React-dom é integração do react com a arvore de elementos (dom)
-import { render } from "react-dom";
-//
-import App from "./App";
-
-// Renderizar o codigo react, dentro de algum conteudo do html.
-render(<App />, document.getElementById("app"));
